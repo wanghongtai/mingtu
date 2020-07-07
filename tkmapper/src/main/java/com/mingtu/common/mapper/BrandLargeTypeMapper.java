00bda0a0e14baf5a26e4be8579ff557451bcc1d6
Hunter <1014398744@qq.com>
2020-07-07 22:00:55 +0800

    商标大类添加小类列表

---
+++ tkmapper/src/main/java/com/mingtu/common/mapper/BrandLargeTypeMapper.java
@@ -0,0 +1,7 @@
+package com.mingtu.common.mapper;
+
+import com.mingtu.common.entity.BrandLargeType;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BrandLargeTypeMapper extends Mapper<BrandLargeType> {
+}
---
+++ tkmapper/src/main/java/com/mingtu/common/mapper/BrandSmallTypeMapper.java
@@ -0,0 +1,7 @@
+package com.mingtu.common.mapper;
+
+import com.mingtu.common.entity.BrandSmallType;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BrandSmallTypeMapper extends Mapper<BrandSmallType> {
+}
