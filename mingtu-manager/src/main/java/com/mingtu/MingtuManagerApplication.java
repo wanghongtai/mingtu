545e4134f38f3d7b460a126226d4801bda405fd5
Hunter <1014398744@qq.com>
2020-07-07 08:57:44 +0800

    添加多环境配置

--- mingtu-manager/src/main/java/com/mingtu/MingtuManagerApplication.java
+++ mingtu-manager/src/main/java/com/mingtu/MingtuManagerApplication.java
@@ -1,5 +1,6 @@
 package com.mingtu;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -9,9 +10,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /*修改启动类，继承SpringBootServletInitializer类并重写configure方法 */
 public class MingtuManagerApplication extends SpringBootServletInitializer {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(MingtuManagerApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(MingtuManagerApplication.class, args);
+	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
